b5e1fae450f8667829788d7c1e45d990b6f1df88
KaiQi <acs110133@gm.ntcu.edu.tw>
2024-03-30 00:36:31 +0800

    feat: add barcode scanner

--- public/shop-machine.js
+++ public/shop-machine.js
@@ -24,4 +24,44 @@ const intervalId = setInterval(() => {
     number++;
     document.getElementById("number").textContent = number;
     generateQRCode(shopID + "&" + number)
-}, 100000000);
+}, 1000000);
+
+
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    // If found your qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        console.log(decodeText);
+        fetch(decodeText, { method: "POST" })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert('轉款成功');
+                    window.location.href = '/scanner.html'; // 導向scanner.html頁面
+                } else {
+                    alert('轉款失敗 ' + data.error);
+                }
+            })
+            .catch(error => {
+                console.error('出事了', error);
+                alert('轉款失敗');
+            });
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
